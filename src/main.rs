778a4b621742a9e67778bb332d734bbaacb7f714
iskry <89371514+iskry@users.noreply.github.com>
2024-04-02 11:47:49 +0000

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,46 @@
-use clap::{Command, Arg};
 mod pcap;
 
+use clap::{Arg, ArgAction, Command};
+use iptables::IPTables;
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let matches = Command::new("Network Tool")
-        .version("1.0")
-        .author("Your Name <your_email@example.com>")
-        .about("Captures network packets and manages iptables rules")
+    let matches = Command::new("IPTABLES Manager")
+        .version("0.1.0")
+        .author("Your Name")
+        .about("Manages iptables rules and captures network traffic")
         .subcommand(
-            Command::new("capture")
-                .about("Captures network packets")
-                .arg(Arg::new("interface")
-                    .short('i')
-                    .long("interface")
-                    .takes_value(true)
-                    .help("Specify the network interface to capture packets from"))
+            Command::new("list").about("Lists iptables rules").arg(
+                Arg::new("table")
+                    // .short('t')
+                    // .long("table")
+                    .required(true)
+                    .index(1)
+                    // .action(ArgAction::Set)
+                    .help("The iptables table to list rules from"),
+            ),
         )
-        .subcommand(
-            Command::new("list")
-                .about("Lists iptables rules")
-                .arg(Arg::new("table")
-                    .help("Specifies the iptables table to list rules from")
-                    .required(false)
-                    .default_value("filter")
-                    .index(1))
-        )
+        .subcommand(Command::new("capture").about("Captures network packets"))
         .get_matches();
 
-    if let Some(capture_matches) = matches.subcommand_matches("capture") {
-        let interface = capture_matches.value_of("interface");
-        pcap::capture_packets(interface)?;
+    if let Some(matches) = matches.subcommand_matches("list") {
+        let table = matches
+            .get_one::<String>("table")
+            .map(|s| s.as_str())
+            .unwrap_or("filter");
+        list_rules(table)?;
+    }
+    if matches.subcommand_matches("capture").is_some() {
+        pcap::capture_packets()?;
     }
 
+    Ok(())
+}
+
+fn list_rules(table: &str) -> Result<(), Box<dyn std::error::Error>> {
+    let ipt = iptables::new(false).unwrap();
+
+    let rules = ipt.list(table, "INPUT")?; // Adjust "INPUT" to the chain you're interested in
+    for rule in rules {
+        println!("{}", rule);
+    }
     Ok(())
 }
